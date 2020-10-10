ac024dbd82ab415e7998d681021ee1f0993cd2f3
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-10-10 23:04:43 +0530

    Markup, Styling and El referencing in JS

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const calculatorDisplay = document.getElementById("calculator-display");
+
+const operatorButtonPlus = document.getElementById("operator-button-plus");
+const operatorButtonMinus = document.getElementById("operator-button-minus");
+const operatorButtonMultiply = document.getElementById(
+  "operator-button-multiply",
+);
+const operatorButtonDivision = document.getElementById(
+  "operator-button-division",
+);
+const operatorButtonEquals = document.getElementById("operator-button-equals");
+
+const numberButton_0 = document.getElementById("number-button-0");
+const numberButton_1 = document.getElementById("number-button-1");
+const numberButton_2 = document.getElementById("number-button-2");
+const numberButton_3 = document.getElementById("number-button-3");
+const numberButton_4 = document.getElementById("number-button-4");
+const numberButton_5 = document.getElementById("number-button-5");
+const numberButton_6 = document.getElementById("number-button-6");
+const numberButton_7 = document.getElementById("number-button-7");
+const numberButton_8 = document.getElementById("number-button-8");
+const numberButton_9 = document.getElementById("number-button-9");
+
+const decimalButton = document.getElementById("decimal-button");
+const clearButton = document.getElementById("clear-button");
